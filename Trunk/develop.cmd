8af509d79b5ee58ac1d516a65e883057cac822f7
SteveIves_cp <SND\SteveIves_cp>
2014-12-04 06:28:39 +0000

    Removed the environment variables from all projects because the mechanism is unreliable.

---
+++ Trunk/develop.cmd
@@ -0,0 +1,19 @@
+@echo off
+set DEVROOT=%~dp0
+call "C:\Program Files\Synergex\SynergyDE\dbl\dblvars64.bat" > nul
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat" > nul
+path=C:\Program Files (x86)\MSBuild\Synergex\dbl;%path%
+prompt CodeGen Trunk $p$g
+
+set RPSDAT=%DEVROOT%SampleRepository\rpsdat
+set RPSMFIL=%RPSDAT%\rpsmain.ism
+set RPSTFIL=%RPSDAT%\rpstext.ism
+set CODEGEN_SRC=%DEVROOT%CodeGenEngine
+set CODEGEN_TPLDIR=%DEVROOT%SampleTemplates
+set CODEGEN_OUTDIR=%DEVROOT%OutputFiles
+set CODEGEN_AUTHOR=Steve Ives
+set CODEGEN_COMPANY=Synergex Professional Services Group
+
+devenv CodeGen.sln
+
+exit
