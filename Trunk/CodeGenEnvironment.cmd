34a40a6bf1ea1b1fc9e7c8514b2a275cf85a21fe
SteveIves_cp <SND\SteveIves_cp>
2015-02-02 16:42:39 +0000

    Improved the trunk environment setup script so that it is pre-configured to use templates in the SampleTemplates folder and create output in the OutputFiles folder.

--- Trunk/CodeGenEnvironment.cmd
+++ Trunk/CodeGenEnvironment.cmd
@@ -6,27 +6,27 @@ rem is currently in the Bin\Release folder. Running this script will override an
 rem that might be installed on the system.
 rem
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
+call "%VS120COMNTOOLS%vsvars32.bat"
 call "%SYNERGYDE64%dbl\dblvars64.bat" > nul
 
 set CODEGEN_ROOT=%~dp0
-
-set CODEGEN_TPLDIR=.
-set CODEGEN_OUTDIR=.
+set CODEGEN_TPLDIR=%CODEGEN_ROOT%SampleTemplates
+set CODEGEN_OUTDIR=%CODEGEN_ROOT%OutputFiles
 set CODEGEN_EXTDIR=%CODEGEN_ROOT%Bin\Release
-
 set RPSDAT=%CODEGEN_ROOT%SampleRepository
 set RPSMFIL=%CODEGEN_ROOT%SampleRepository\rpsmain.ism
 set RPSTFIL=%CODEGEN_ROOT%SampleRepository\rpstext.ism
 
 PATH=%CODEGEN_ROOT%Bin\Release;%ProgramFiles(x86)%\MSBuild\Synergex\dbl;%ProgramFiles(x86)%\WiX Toolset v3.9\bin;%PATH%
 
-cd /d %CODEGEN_ROOT%
+cd /d %CODEGEN_OUTDIR%
 
 cls
 echo.
 codegen -version
 echo.
-echo Template folder : Current directory
-echo Output folder   : Current directory
+echo CodeGen Executable : %CODEGEN_ROOT%Bin\Release\codegen.exe
+echo Repository         : %RPSMFIL%
+echo Template folder    : %CODEGEN_TPLDIR%
+echo Output folder      : %CODEGEN_OUTDIR%
 echo.
