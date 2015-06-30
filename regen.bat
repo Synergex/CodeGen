36a18771e09e200238dbf644162fc05c5ea6948c
Steve Ives <Steve.Ives@synergex.com>
2015-06-30 02:51:24 +0000

    Updated Orchestrator batch file to match .synproj file.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS35]$/CodeGen/Trunk;C41001

--- regen.bat
+++ regen.bat
@@ -20,7 +20,7 @@ pushd "%ROOT%SymphonyOrchestratorLibrary"
 set OPT=-e -r -lf
 set CODEGEN_TPLDIR=%SYMPHONYTPL%
 
-set STR_DATA=CODEGEN_STRUCTURES CODEGEN_COMMAND CODEGEN_USER_TOKEN EXECUTION_RESULTS ORCHESTRATOR_DEFAULTS
+set STR_DATA=CODEGEN_STRUCTURES CODEGEN_COMMAND CODEGEN_USER_TOKEN EXECUTION_RESULTS ORCHESTRATOR_DEFAULTS ORCHESTRATOR_PROJECT
 set STR_STYLE=CODEGEN_STRUCTURES CODEGEN_COMMAND ORCHESTRATOR_DEFAULTS ORCHESTRATOR_PROJECT
 set STR_CONTENT=CODEGEN_STRUCTURES CODEGEN_COMMAND ORCHESTRATOR_DEFAULTS ORCHESTRATOR_PROJECT
 set STR_COLLECTION=CODEGEN_COMMAND ORCHESTRATOR_PROJECT
@@ -34,11 +34,11 @@ tf checkout /recursive *.CodeGen.* /login:%CODEPLEX_UID%,%CODEPLEX_PWD% > nul
 echo.
 echo Regenerating code...
 
-codegen %OPT% -s %STR_DATA%       -t Symphony_DataNoExcel -o .\Model     -n Symphony.Orchestrator.Library.Model -prefix m
-codegen %OPT% -s %STR_STYLE%      -t Symphony_Style       -o .\Resources -cw 16 -ut ASSEMBLYNAME=SymphonyOrchestratorLibrary ANCESTORCONTROL=Window
-codegen %OPT% -s %STR_CONTENT%    -t Symphony_Content     -o .\Resources -n Symphony.Orchestrator.Library.Content -ut ASSEMBLYNAME=SymphonyOrchestratorLibrary
-codegen %OPT% -s %STR_COLLECTION% -t Symphony_Collection  -o .\Content   -n Symphony.Orchestrator.Library.Content
-codegen %OPT% -s %STR_ASXML%      -t Symphony_AsXML       -o .\Model     -n Symphony.Orchestrator.Library.Model
+codegen %OPT% -s %STR_DATA%       -t Symphony_DataNoExcel -o .\Model     -prefix m -n Symphony.Orchestrator.Library.Model
+codegen %OPT% -s %STR_STYLE%      -t Symphony_Style       -o .\Resources -prefix m -n Symphony.Orchestrator.Library         -ut ASSEMBLYNAME=SymphonyOrchestratorLibrary ANCESTORCONTROL=Window -cw 16
+codegen %OPT% -s %STR_CONTENT%    -t Symphony_Content     -o .\Resources -prefix m -n Symphony.Orchestrator.Library.Content -ut ASSEMBLYNAME=SymphonyOrchestratorLibrary
+codegen %OPT% -s %STR_COLLECTION% -t Symphony_Collection  -o .\Content   -prefix m -n Symphony.Orchestrator.Library.Content 
+codegen %OPT% -s %STR_ASXML%      -t Symphony_AsXML       -o .\Model     -prefix m -n Symphony.Orchestrator.Library.Model
 
 echo.
 echo Code generation complete
