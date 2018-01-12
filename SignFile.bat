60fed4c93be78452244bca3b0fa4e57ccbbace3c
Steve Ives <steve.ives@synergex.com>
2018-01-12 11:11:10 -0800

    Corrected SignFile.bat for use with new key signing process.

--- SignFile.bat
+++ SignFile.bat
@@ -11,27 +11,6 @@ if not exist "%FILE_TO_SIGN%" (
   goto done
 )
 
-if not defined CODE_SIGN_CERT (
-  echo ERROR: No code signing certificate is defined. Set environment varaible CODE_SIGN_CERT.
-  goto done
-)
-
-if not exist "%CODE_SIGN_CERT%" (
-  echo ERROR: Code signing certificate %CODE_SIGN_CERT% was not found!
-  goto done
-)
-
-set PASSWORD=%2
-if not "%PASSWORD%"=="" goto gotPassword
-
-set PASSWORD=%CODE_SIGN_PWD%
-if "%PASSWORD%"=="" (
-  echo ERROR: Code signing password is not defined. Set environment variable CODE_SIGN_PWD.
-  goto done  
-)
-
-:gotPassword
-
 set TIMESTAMP_URL=http://timestamp.entrust.net/TSS/AuthenticodeTS
 
 echo.
@@ -50,7 +29,7 @@ goto done
 
 :usage
 echo.
-echo Usage: SignFile file [password]
+echo Usage: SignFile <fileSpec>
 echo.
 
 :done
