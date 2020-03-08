d7a49fe5bd60938541700f15d66144c2017f7155
Steve Ives <steve@steveives.com>
2020-03-08 12:50:53 -0700

    Updated SignTool.exe location.

--- SignFile.bat
+++ SignFile.bat
@@ -19,7 +19,7 @@ for %%F in ("%FILE_TO_SIGN%") do echo Signing %%~nxF
 rem Should be able to use %WindowsSdkDir% but it looks like Visual Studio clears it for some reason!
 
 rem This is the command used with the certificate on the physical USB device.
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x86\signtool.exe" sign /fd SHA256 /a /tr "%TIMESTAMP_URL%" "%FILE_TO_SIGN%"
+"C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool.exe" sign /fd SHA256 /a /tr "%TIMESTAMP_URL%" "%FILE_TO_SIGN%"
 
 if "%ERRORLEVEL%"=="0" (
   echo SUCCESS!
